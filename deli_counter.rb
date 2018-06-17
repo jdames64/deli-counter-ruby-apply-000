6333463d2ca5fab5be127a5d673cfb4c6452d4b6
j.d.ames64@gmail.com <j.d.ames64@gmail.com>
2018-06-17 21:55:12 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,17 @@
 # Write your code here.
-katz_deli = []
 
-def line(array, name)
-  if katz_deli == 0
-    puts "The line is currently empty."
-  end
+def line(array)
+ if array.length == 0
+   puts "The line is currently empty."
+ else
+ index = 1
+ new_array = []
+ array.each do |name|
+ new_array.push "#{index}. #{name}"
+ index += 1
+ end
+ puts "The line is currently: #{new_array}" 
+   end
 end
 
 def take_a_number(array, string)
