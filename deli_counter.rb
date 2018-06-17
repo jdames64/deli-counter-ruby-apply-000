f0160efea00d6a60dd715ac7eae079f799e6846c
j.d.ames64@gmail.com <j.d.ames64@gmail.com>
2018-06-17 21:57:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,17 +1,18 @@
 # Write your code here.
 
 def line(array)
- if array.length == 0
-   puts "The line is currently empty."
- else
- index = 1
- new_array = []
- array.each do |name|
- new_array.push "#{index}. #{name}"
- index += 1
- end
- puts "The line is currently: #{new_array}"
-   end
+if array.length == 0
+  puts "The line is currently empty."
+else
+index = 1
+new_array = []
+array.each do |name|
+new_array.push "#{index}. #{name}"
+index += 1
+end
+puts "The line is currently: "
+puts new_array
+  end
 end
 
 def take_a_number(array, string)
