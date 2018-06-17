890ebb46b5468e89e1ef362af8f421dbb2f556fa
j.d.ames64@gmail.com <j.d.ames64@gmail.com>
2018-06-17 22:16:18 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -4,12 +4,12 @@ def line(array)
    if array.length == 0
      puts "The line is currently empty."
    else
-     print "The line is currently: "
+     print "The line is currently:"
      index = 0
      new_array = []
      array.each do |name|
-     new_array.push "#{index+1}. #{name}"
-     puts "#{new_array[index]}"
+     new_array.push " #{index+1}. #{name}"
+     print "#{new_array[index]}"
      index += 1
    end
  end
